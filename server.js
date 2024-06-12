ea2efeb2e56356ba50445512b3d117a67d25d8f3
capstone-ryker <obkiss1@naver.com>
2024-06-12 17:16:35 +0900

    Update server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.post('/notify', (req, res) => {
 });
 
 // 서버 시작
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001; // 다른 포트 번호 사용
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
