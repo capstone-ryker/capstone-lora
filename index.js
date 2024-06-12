b671c6dc74c62e5445f7622155c0071ed77767f1
capstone-ryker <obkiss1@naver.com>
2024-06-12 16:50:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/notify', (req, res) => {
     const message = req.body.message;
-    // 받은 메시지를 콘솔에 출력하고 응답을 반환합니다.
     console.log(`Received notification: ${message}`);
     res.json({ status: 'success', message: 'Notification received' });
 });
